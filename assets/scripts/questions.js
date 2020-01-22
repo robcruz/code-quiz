28a99b469afb4335be84d45e90ca540ddf530d74
Rob Cruz <alekseicruz@gmail.com>
2020-01-21 22:07:20 -0800

    Initial checkin

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -16,11 +16,17 @@ let contentElement = document.querySelector('#content')
 
 let h3 = document.createElement('h3')
 let p = document.createElement('p')
+let div = document.createElement('div')
+let startQuizButton = document.createElement('button')
 
 contentElement.appendChild(h3)
 contentElement.appendChild(p)
+contentElement.appendChild(div)
+div.appendChild(startQuizButton)
 
 h3.textContent = `Coding Quiz Challenge`
 p.textContent = 'Try to answer the following code related questions within the time frame limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!'
 p.setAttribute('class', 'button-style')
-
+div.setAttribute('class', 'button-style')
+startQuizButton.setAttribute('class', 'btn btn-sm')
+startQuizButton.textContent = 'Start Quiz'
