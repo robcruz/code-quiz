c6492c9989bcee35b08da5ad91ef93b0641bb96e
Rob Cruz <alekseicruz@gmail.com>
2020-01-21 22:11:46 -0800

    Initial checkin

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -12,21 +12,27 @@ var questions = [
   ///etc.
 ];
 
-let contentElement = document.querySelector('#content')
+let content = document.querySelector('#content')
 
-let h3 = document.createElement('h3')
-let p = document.createElement('p')
-let div = document.createElement('div')
-let startQuizButton = document.createElement('button')
+landingPage(content)
 
-contentElement.appendChild(h3)
-contentElement.appendChild(p)
-contentElement.appendChild(div)
-div.appendChild(startQuizButton)
+function landingPage(content){
+  let h3 = document.createElement('h3')
+  let p = document.createElement('p')
+  let div = document.createElement('div')
+  let startQuizButton = document.createElement('button')
+  content.appendChild(h3)
+  content.appendChild(p)
+  content.appendChild(div)
+  div.appendChild(startQuizButton)
 
-h3.textContent = `Coding Quiz Challenge`
-p.textContent = 'Try to answer the following code related questions within the time frame limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!'
-p.setAttribute('class', 'button-style')
-div.setAttribute('class', 'button-style')
-startQuizButton.setAttribute('class', 'btn btn-sm')
-startQuizButton.textContent = 'Start Quiz'
+  h3.textContent = `Coding Quiz Challenge`
+  p.innerText = 'Try to answer the following code related questions within the time frame limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!'
+  p.setAttribute('class', 'button-style')
+  div.setAttribute('class', 'button-style')
+  startQuizButton.setAttribute('class', 'btn btn-sm')
+  startQuizButton.textContent = 'Start Quiz'
+}
+
+
+
