08d75e2d464bc98b812cf2a8bd588e4b0dfb74c6
Rob Cruz <alekseicruz@gmail.com>
2020-01-25 04:23:28 -0800

    Initial checkin

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -12,63 +12,31 @@ let questions = [
   ///etc.
 ];
 
-let content = document.querySelector('#content')
-let h3 = document.createElement('h3')
-let p = document.createElement('p')
-let div = document.createElement('div')
-let startQuizButton = document.createElement('button')
-let answerArr = []
-let button = document.createElement('button')
+let questionsArrayIndex = 0
+let startContainer = document.querySelector('#start-container')
+let questionElement = document.querySelector('#question')
 
-let questionsIndex = 0
+loadEventListeners()
 
-
-
-startQuizButton.addEventListener('click', startQuiz)
-
-
-loadLandingPage()
-
-
-function startQuiz(event){
-  event.preventDefault()
-  h3.remove()
-  p.remove()
-  div.remove()
-
-  let h4 = document.createElement('h4')
-  button.textContent = 'continue'
-
-
-  content.appendChild(h4)
-  h4.innerHTML = questions[questionsIndex]['title']
-
-  let br = document.createElement('BR')
-  questions[questionsIndex]['choices'].forEach(choice => {
-    let button = document.createElement('button')
-    content.appendChild(button)
-    button.innerText = choice
-    // div.appendChild(document.createElement('BR'))
-    button.append(document.createElement('BR'))
-    // button.append(document.createElement('br'))
-    // document.write('<br/>')
+function loadEventListeners(){
+  startContainer.addEventListener('click', function (event) {
+    startContainer.remove()
+    renderQuestion(questionsArrayIndex)
   })
 
 
 }
 
-function loadLandingPage() {
-  content.appendChild(h3)
-  content.appendChild(p)
-  content.appendChild(div)
-  div.appendChild(startQuizButton)
+function renderQuestion(index) {
+  let question = questions[index]
+  questionElement.textContent = question['title']
 
-  div.setAttribute('class', 'button-style')
+  question['choices'].forEach((choice, index) => {
 
-  h3.textContent = `Coding Quiz Challenge`
-  p.innerText = ''
-  p.setAttribute('class', 'button-style')
-  startQuizButton.setAttribute('class', 'btn btn-sm')
-  startQuizButton.textContent = 'Start Quiz'
+  })
+  question['answer']
+
+  questionsArrayIndex++
 }
+
 
